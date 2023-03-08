f3351ffa9ab9259f98ac24f21d9c13f35d805339
FedePorta <federico.porta1197@gmail.com>
2023-03-08 20:12:40 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,108 +1,121 @@
+let productos = [
+    { id: 1, nombre: "Air Jordan I", precio: 7000, stock:20 },
+    { id: 2, nombre: "Air Jordan II", precio: 7990, stock:1},
+    { id: 3, nombre: "Air Jordan III", precio: 6500, stock: 12 },
+    { id: 4, nombre: "Air Jordan VI", precio: 8990, stock:3 },
+  ];
+
+let pagos = [
+    {id: 1, nombre: "Santander", descuento: 25},
+    {id: 2, nombre: "Itau", descuento: 30},
+    {id: 3, nombre: "OCA", descuento: 15},
+    {id: 4, nombre: "Efectivo", descuento: 0},
+]
+
+const carrito = [];
 
 
-
-let producto_1=550;
-let producto_2=640;
-let producto_3=380;
 
 let subtotal = 0;
-let compra ="si";
+let compra = true;
+let objeto;
 
-
-function Bienvenido(compra){
-    alert("Bienvenido, te recordamos los descuentos que tienes con las siguientes tarjetas: \nSantander 25% OFF\n Itaú 15% OFF\n OCA 30% OFF");
-    Compra();
-}   
-
-
-while (compra=="si"){
+while (compra){
     Bienvenido();
 }
+
+function Bienvenido(compra){
+
+    objeto = buscar(pagos)
+    let mensaje = "Bienvenido, te recordamos los descuentos que tienes con las siguientes tarjetas: \n \n" + objeto;
+    alert(mensaje);
+
+    Compra();
+} 
+
+
+
+function buscar(item,id){
+   
+    if (id) {
+       
+        let buscador=(item.find(item => item.id == id))
+
+        if(buscador){return buscador; }
+    }
+    else{
+        let salida = "";
+            for (let i = 0; i < item.length; i++) {
+                for(let clave in item[i]) {
+                    salida += `${clave}: ${item[i][clave]}\n`;
+  }
+  salida += "\n";
+}
+return(salida);
+    }
+}
+
+
+
+
+
 
 
 function Compra(){
 
-let entrada = prompt("Dinos que producto te gustaria adquirir\n Nike AirForce 1 / $550\n Nike Jordan I / $640\n Nike Jordan IV / $380\n");
-let cantidad = prompt("Cuantas desea llevar?");
+    
+        let entrada = prompt("Dinos que producto te gustaria adquirir\n\n ingresa su ID \n\n" + buscar(productos));
+        let cantidad = prompt("Cuantas desea llevar?");
 
-
-if(entrada <=0 || entrada >=4  ){
-    alert(entrada+ " No esta dentro de las opciones");
-    Compra();}
-
-    else if (cantidad >=5){
-    alert("La cantidad supera el stock")
-    Compra();}
-
-    else{ subtotal= subtotal+=calcularsubtotal(entrada,cantidad);}
-
-let opcional= prompt("Deseas llevar otro producto? (si/no)")
-let total;
-        if(opcional=="si")Compra();
-        else {
-            let tarjeta = prompt("Cual seria el metodo de pago? \n 1-Santander 25% OFF\n 2-Itaú 15% OFF\n 3-OCA 30% OFF\n 4-Efectivo");
-
-            total=calculartotal(subtotal,tarjeta)
+        objeto = buscar(productos,entrada)
+    
+            if(validarstock(entrada, cantidad)){
+                alert("el Producto "+ objeto.nombre +" se agrego exitosamente a su carro");
+                carrito.push(objeto.precio);
+                console.log(carrito);
+                continuarcompra();
+            }
+            else{
+                alert("El producto " + objeto.nombre + " no cuenta con suficiente stock " + + objeto.stock)
+                Compra();
             }
 
-
-
-alert("su total es "+total)
-return (compra="no");
+        return (compra=false);
 }
 
 
 
-
-function calcularsubtotal(entrada, cantidad){
-let calculo;
-    switch(entrada){
-           case "1":
-             console.log("eligio producto 1");
-             calculo= producto_1*cantidad
-
-            break;
-
-            case "2":
-                console.log("eligio producto 2");
-             calculo= producto_2*cantidad
-   
-            break;
-
-            case "3":
-                console.log("eligio producto 2");
-             calculo= producto_3*cantidad
-       
-            break;
-       
-         }
-         return calculo;
+function validarstock(entrada,cantidad) {
+    let salida= buscar(productos,entrada);
+        if(salida.stock>=cantidad){
+          return true;
+        }
+    return false;
 }
 
-function calculartotal(subtotal, tarjeta){
-    let descuento;
-    switch(tarjeta){
-        case "1":
-            console.log("Santander");
-            descuento = subtotal*0.20
-         break;
+function continuarcompra(){
 
-         case "2":
-             console.log("eligio Itaú");
-             descuento=subtotal*0.15;
+    let opcional= prompt("Deseas llevar otro producto? \n \ 1= si \n 2= no)")
+    if (opcional==1) Compra();
+    else calcularsubtotal();
+}
 
-         break;
 
-         case "3":
-             console.log("eligio OCA");
-             descuento=subtotal*0.30;
+function calcularsubtotal(){
+
+    let metodopago= prompt("Elije el metodo de pago que deseas implementar " +buscar(pagos))
     
-         break;
-         
-         case "4":
-             console.log("eligio Efectivo");  
-             descuento=0;
-         break;
-      }
-      return (subtotal-descuento);
-}
+    let subtotal= buscar(pagos,metodopago);
+    let total=0;
+    for(let x=0; x<carrito.length; x++){
+        total+=Number(carrito[x]);}
+
+        total= total*((100-subtotal.descuento)/100);
+        alert("Su total de Compra es de "+ total+ " Gracias por su compra.");
+        compra=false;
+        return;
+    }
+ 
+    
+
+    
