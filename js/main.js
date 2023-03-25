493e4470457510010f2121ee27fd148f16caa8f2
FedePorta <federico.porta1197@gmail.com>
2023-03-25 16:20:41 -0300

    redirect no funcav2

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ const productoss = document.getElementById('productos');
 function compradirecta(id){
     
     addcarro(id);
-    window.location.href = "./vw/carrito.html";
+    window.location.href = "JS/vw/carrito.html";
   
 
 }
